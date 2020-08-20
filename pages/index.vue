ca196ab491b40b3440c0ce06a59ff407747a81cf
Dominic <partner.dominic@hotmail.com>
2020-08-19 20:59:12 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="container">
+    <div>
+      <Logo />
+      <h1 class="title">nuxt-fundamentals</h1>
+      <div class="links">
+        <nuxt-link 
+          v-for="post in posts"
+          :key="post.id"
+          :to="{name:'posts-id', params: {id: post.id}}"
+          class="button--grey">
+          {{ post.title }}
+          </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+  head() {
+    return {
+      title: 'Home page ⚡️',
+      meta: [
+        {name: 'twitter:title', content: 'Nuxt fudamentals'},
+        {name: 'twitter:description', content: 'Nuxt + Vue = ⚡️'},
+        {name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png'},
+        {name: 'twitter:card', content: 'summary_large_image'}
+      ]
+    }
+  },
+
+  computed: {
+    posts() {
+      return this.$store.state.posts.all;
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  letter-spacing: 1px;
+}
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+}
+.links {
+  padding-top: 15px;
+}
+</style>
